f22d57b22863ab1e550e83da28bbdd68c45bfd66
amirrezaask <raskarpour@gmail.com>
2021-09-13 00:50:48 +0430

    initialize

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+/*
+* Welcome is a simple prompt welcome program. You basically call this before your actual prompt
+* when your shell starts and it will print based on your configuration
+*
+* */
+
+fn main() {}
